56d2c94197585e62f1dc2216d1c168b928d87b60
Ankur Goswami <ankur.goswami12@gmail.com>
2018-12-03 02:34:56 -0600

    Create MaskRCNN pynb

--- exp/demo.py
+++ exp/demo.py
@@ -29,17 +29,19 @@ def rect(d, im, color, points):
 	for i, pt in enumerate(points[:-1]):
 		d.line((pt, points[i+1]), fill=color, width=4)
 
-def draw_rois(d, im, color, rois):
+def draw_rois(d, im, color, rois, cls):
 	for idx, roi in enumerate(rois):
 		y1, x1, y2, x2 = roi
 		points = (x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)
+                text_points = (x1, y1)
+                d.text(text_point, cls[idx])
 		rect(d, im, color, points )
 
 def draw_image(name, path, rois,gt_rois, classes):
 	im = Image.open(path)
 	d = ImageDraw.Draw(im)
-	draw_rois(d, im, "#f00", rois)
-	draw_rois(d, im, "#00f", gt_rois)
+	draw_rois(d, im, "#f00", rois, classes)
+	draw_rois(d, im, "#00f", gt_rois, classes)
 	im.save(f"{name}.png", "png")
 
 inference_config = InferenceConfig()
@@ -65,6 +67,7 @@ for idx, image_id in enumerate(tqdm(image_ids)):
 	image, image_meta, gt_class_id, gt_bbox, gt_mask =\
         modellib.load_image_gt(data_test, inference_config,image_id, use_mini_mask=False) 
 	results = model.detect([image], verbose=0)
+        print(r)
 	r = results[0]
 	info = data_test.image_info[image_id]
 	draw_image(info["str_id"], info["path"], r["rois"], gt_bbox,None)
