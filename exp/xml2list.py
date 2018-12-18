b5821a1ce86cdfad2659d9ee38b30b99c6c71165
Josh McGrath <joshuawmcgrath@gmail.com>
2018-12-18 11:31:45 -0600

    added xml to list layer

---
+++ exp/xml2list.py
@@ -0,0 +1,27 @@
+"""
+convert VOC XML to a list
+"""
+from xml.etree import ElementTree as ET
+
+def mapper(obj):
+    """
+    map a single object to the list structure
+    :param obj: an Etree node
+    :return: (type, (x1, y1, x2, y2))
+    """
+    bnd = obj.find("bndbox")
+    coords = ["xmin", "ymin", "xmax", "ymax"]
+    return obj.find("name").text, [int(float(bnd.find(coord).text)) for coord in coords]
+
+def xml2list(fp):
+    """
+    convert VOC XML to a list
+    :param fp: file path to VOC XML file
+    :return: [(type,(x1, y1, x2, y2))]
+    """
+    tree = ET.parse(fp)
+    root = tree.getroot()
+    objects = root.findall("object")
+    return [mapper(obj) for obj in objects]
+
+
