9b485ff83b880cdabac5618e6d1c1e5b8caf778e
Ankur Goswami <ankur.goswami12@gmail.com>
2019-01-16 15:22:23 -0600

    Flesh out evaluation and statistics a bit Edit xml2list to make sure the difficulty tag exists on the object

--- exp/evaluate.py
+++ exp/evaluate.py
@@ -76,8 +76,73 @@ def run_past_eval():
     	print(f"{AP_key} AP: {np.mean(APs[AP_key])}")
     	print(f"{AP_key} IoU: {np.mean(ious[AP_key])}")
 
-def run_evaluate():
-    pass
+def calculate_iou(box1, box2):
+    # Shamelessly adapted from
+    # https://stackoverflow.com/questions/25349178/calculating-percentage-of-bounding-box-overlap-for-image-detector-evaluation
+    # determine the coordinates of the intersection rectangle
+    x_left = max(box1[0], box2[0])
+    y_top = max(box1[1], box2[1])
+    x_right = min(box1[2], box2[2])
+    y_bottom = min(box1[3], box2[3])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    bb1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    bb2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    return iou
+
+
+def match_lists(predict_list, target_list):
+    list_map = {}
+    for prediction in prediction_list:
+        p_cls, p_bb, p_score = prediction
+        # Calculate which output annotation has the highest IOU
+        ious = [calculate_iou(p_bb, target[1]) for target in target_list]
+        if len(ious) == 0:
+            list_map[prediction] = None
+        else:
+            max_iou = max(ious)
+            for ind, iou in enumerate(ious):
+                if iou == max_iou:
+                    list_map[prediction] = target_list[ind]
+                    break
+    return list_map
+
+
+
+def run_evaluate(predict_dir, target_dir):
+    statistics = {
+        'background': 0
+        'total_predictions': 0
+    }
+    for predict_f in os.listdir(predict_dir):
+        predict_path = os.path.join(predict_dir, predict_f)
+        target_path = os.path.join(target_dir, predict_f)
+        predict_list = xml2list(predict_path)
+        target_list = xml2list(target_path)
+        list_map = match_lists(predict_list, target_list)
+        for predict in predict_list:
+            matched_target = list_map[predict]
+            statistics['total_predictions'] += 1
+            if matched_target is None:
+                statistics['background'] += 1
+                continue
+            p_cls, p_bb, p_score = predict
+            t_cls, t_bb, t_score = target
+            if p_cls == t_cls:
+                pass
+
 
 if __name__ == '__main__':
     run_evaluate()
