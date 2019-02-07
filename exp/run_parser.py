9b666d54298c28cb8e1d4ed842df68c63eb97ff5
Ankur Goswami <ankur.goswami12@gmail.com>
2019-02-07 15:16:12 -0600

    Migrate parser ingestion settings to config.py

--- exp/run_parser.py
+++ exp/run_parser.py
@@ -1,16 +1,17 @@
 from Parser.parse_html_to_postgres import parse_html_to_postgres
+from config import ingestion_settings as settings
 
 if __name__ == '__main__':
-    input_folder = '/home/paulluh/Cosmos/exp/data4parser/html/files/'
+    input_folder = settings['input_folder']
 
     # intermediate folder location (will be auto-generated)
-    merge_folder = '/home/paulluh/Cosmos/exp/data4parser/html/merged/'
-    output_html = '/home/paulluh/Cosmos/exp/data4parser_out/html/'
-    output_words = '/home/paulluh/Cosmos/exp/data4parser_out/words/'
+    merge_folder = settings['merge_folder']
+    output_html = settings['output_html']
+    output_words = settings['output_words']
 
-    db_connect_str = 'postgres://postgres:password@localhost:5432/cosmos7'
+    db_connect_str = settings['db_connect_str']
 
-    strip_tags = ['strong', 'em']
-    ignored_file_when_link = []
+    strip_tags = settings['strip_tags']
+    ignored_file_when_link = settings['ignored_file_when_link']
 
     parse_html_to_postgres(input_folder, output_html, merge_folder, output_html, db_connect_str, strip_tags, ignored_file_when_link)
