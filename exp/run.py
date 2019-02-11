2b04ef3e1b90f215bd382784823107dce717a911
Ian Ross <ian.ackerman.ross@gmail.com>
2019-02-11 10:39:21 -0600

    shift where work happens

--- exp/run.py
+++ exp/run.py
@@ -31,49 +31,54 @@ parser.add_argument('-d', "--weightsdir", default='weights', type=str, help="Pat
 parser.add_argument('-w', "--weights", type=str, help='Path to weights file', required=True)
 parser.add_argument('-t', "--threads", default=160, type=int, help="Number of threads to use")
 parser.add_argument('-n', "--noingest", help="Ingest html documents and create postgres database")
-parser.add_argument('-o', "--output", help="Output directory")
+parser.add_argument('-o', "--output", default='./', help="Output directory")
+parser.add_argument('-p', "--tmp_path", default='tmp', help="Path to directory for temporary files")
 
 args = parser.parse_args()
-#if not os.path.exists('tmp'):
+#i not os.path.exists('tmp'):
 #    os.makedirs('tmp')
 #    os.makedirs('tmp/images')
 #    os.makedirs('tmp/images2')
 #    os.makedirs('tmp/images3')
 
-if not os.path.exists('tmp'):
-    os.makedirs('tmp')
-    os.makedirs('tmp/images')
-    os.makedirs('tmp/images2')
-    os.makedirs('tmp/cc_proposals')
+tmp = args.tmp_path
+xml = os.path.join(args.output, "xml")
+html = os.path.join(args.output, "html")
 
-img_d = os.path.join('tmp', 'images2')
+if not os.path.exists(tmp):
+    os.makedirs(tmp)
+    os.makedirs('%s/images' % tmp)
+    os.makedirs('%s/images2' % tmp)
+    os.makedirs('%s/cc_proposals' % tmp)
+
+img_d = os.path.join(tmp, 'images2')
 def preprocess_pdfs(pdf_path):
     subprocess.run(['gs', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4',
-                    '-dTextAlphaBits=4', '-r600', f'-sOutputFile="tmp/images/{pdf_path}_%d.png"', os.path.join(args.pdfdir, pdf_path)])
+                    '-dTextAlphaBits=4', '-r600', f'-sOutputFile="{tmp}/images/{pdf_path}_%d.png"', os.path.join(args.pdfdir, pdf_path)])
     #subprocess.run(['convert', '-density', '100', '-trim', os.path.join(args.pdfdir, pdf_path), '-quality', '100',
     #                '-sharpen', '0x1.0', os.path.join('tmp','images', f'{pdf_path}-%04d.png')])
 
 def resize_pngs(img_path):
-    path, im = pp.resize_png(os.path.join('tmp', 'images', img_path))
+    path, im = pp.resize_png(os.path.join(f'{tmp}', 'images', img_path))
     if path is not None:
-        im.save(os.path.join('tmp', 'images', img_path))
+        im.save(os.path.join(f'{tmp}', 'images', img_path))
 
 def flatten_png(img_f):
-    subprocess.run(['convert', '-flatten', os.path.join('tmp', 'images', img_f), os.path.join('tmp', 'images', img_f)])
+    subprocess.run(['convert', '-flatten', os.path.join(f'{tmp}', 'images', img_f), os.path.join(f'{tmp}', 'images', img_f)])
 
 def preprocess_pngs(img_f):
-    pth, padded_img = pp.pad_image(os.path.join('tmp', 'images', img_f))
+    pth, padded_img = pp.pad_image(os.path.join(f'{tmp}', 'images', img_f))
     if pth is not None:
         padded_img.save(os.path.join(img_d, img_f))
 
 def convert_to_html(xml_f):
-    xpath = os.path.join('xml', xml_f)
+    xpath = os.path.join(xml, xml_f)
     l = xml2list(xpath)
-    list2html(l, f'{xml_f[:-4]}.png', img_d, 'html')
+    list2html(l, f'{xml_f[:-4]}.png', img_d, html)
 
 def match_proposal(proposal_f):
-    proposal_f_full = os.path.join('tmp', 'cc_proposals', proposal_f)
-    xml_f = f'xml/{proposal_f[:-4]}' + '.xml'
+    proposal_f_full = os.path.join(f'{tmp}', 'cc_proposals', proposal_f)
+    xml_f = f'{xml}/{proposal_f[:-4]}' + '.xml'
     process_doc(xml_f, proposal_f_full, xml_f)
 #for pdf_f in os.listdir(args.pdfdir):
 #    subprocess.run(['gs', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4',
@@ -89,12 +94,12 @@ pool = mp.Pool(processes=args.threads)
 results = [pool.apply_async(preprocess_pdfs, args=(x,)) for x in os.listdir(args.pdfdir)]
 [r.get() for r in results]
 
-results = [pool.apply_async(resize_pngs, args=(x,)) for x in os.listdir(os.path.join('tmp', 'images'))]
+results = [pool.apply_async(resize_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
 [r.get() for r in results]
 
 print('Begin writing proposals')
 #[write_proposals(os.path.join('tmp', 'images', x)) for x in os.listdir(os.path.join('tmp', 'images'))]
-results = [pool.apply_async(write_proposals, args=(os.path.join('tmp', 'images', x),)) for x in os.listdir(os.path.join('tmp', 'images'))]
+results = [pool.apply_async(write_proposals, args=(os.path.join(f'{tmp}', 'images', x),)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
 [r.get() for r in results]
 
 #for img_f in os.listdir('tmp/images2'):
@@ -104,15 +109,15 @@ results = [pool.apply_async(write_proposals, args=(os.path.join('tmp', 'images',
 #    padded_img.save(os.path.join('tmp', 'images3', img_f))
 
 print('Begin preprocessing pngs')
-results = [pool.apply_async(preprocess_pngs, args=(x,)) for x in os.listdir(os.path.join('tmp', 'images'))]
+results = [pool.apply_async(preprocess_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
 [r.get() for r in results]
-shutil.rmtree('tmp/images2')
+shutil.rmtree(f'{tmp}/images2')
 
 with open('test.txt', 'w') as wf:
-    for f in os.listdir('tmp/images'):
+    for f in os.listdir(f'{tmp}/images'):
         wf.write(f[:-4] + '\n')
 
-shutil.move('test.txt', 'tmp/test.txt')
+shutil.move('test.txt', f'{tmp}/test.txt')
 
 
 #class InferenceConfig(Config):
@@ -141,14 +146,14 @@ model = modellib.MaskRCNN(mode="inference",
 #model_path = model.find_last()
 #print("Loading weights from ", model_path)
 model.load_weights(args.weights, by_name=True)
-data_test = PageDataset('test', 'tmp', 0, nomask=True)
+data_test = PageDataset('test', f'{tmp}', 0, nomask=True)
 #data_test.load_page(classes=['Figure', 'Table', 'Equation', 'Body Text'])
 data_test.load_page(classes=list(ICDAR_convert.keys()))
 data_test.prepare()
 image_ids = data_test.image_ids
 
-if not os.path.exists('xml'):
-    os.makedirs('xml')
+if not os.path.exists(xml):
+    os.makedirs(xml)
 
 for idx, image_id in enumerate(tqdm(image_ids)):
     # Load image and ground truth data
@@ -158,15 +163,15 @@ for idx, image_id in enumerate(tqdm(image_ids)):
     r = results[0]
     info = data_test.image_info[image_id]
     zipped = zip(r["class_ids"], r["rois"])
-    model2xml(info["str_id"], 'xml', [1920, 1920], zipped, data_test.class_names, r['scores'])
+    model2xml(info["str_id"], xml, [1920, 1920], zipped, data_test.class_names, r['scores'])
 
-results = [pool.apply_async(match_proposal, args=(x,)) for x in os.listdir(os.path.join('tmp', 'cc_proposals'))]
+results = [pool.apply_async(match_proposal, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'cc_proposals'))]
 [r.get() for r in results]
 
-for xml_f in os.listdir('xml'):
-    xpath = os.path.join('xml', xml_f)
+for xml_f in os.listdir(xml):
+    xpath = os.path.join(xml, xml_f)
     l = xml2list(xpath)
-    list2html(l, f'{xml_f[:-4]}.png', 'tmp/images', 'html')
+    list2html(l, f'{xml_f[:-4]}.png', f'{tmp}/images', html)
 
 
 # Parse html files to postgres db
@@ -189,9 +194,4 @@ else:
 
 
 #shutil.rmtree('xml')
-shutil.rmtree('tmp')
-
-if args.output is not None:
-    shutil.move("xml/", args.output)
-    shutil.move("html/", args.output)
-    shutil.move("html_out/", args.output)
+shutil.rmtree(f'{tmp}')
