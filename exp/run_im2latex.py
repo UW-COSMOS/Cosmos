6be3e8028e7016ca61e95fffd83be77d87a61f02
Paul Luh <paul841029@gmail.com>
2019-02-12 12:18:00 -0600

    Update run_im2latex.py

--- exp/run_im2latex.py
+++ exp/run_im2latex.py
@@ -3,7 +3,7 @@ from latex_ocr.img2latex import img2latex_api
 if __name__ == '__main__':
 
     # Download weights from https://drive.google.com/file/d/1nad6PhxSkGRiWM9WHdKjOrBAqu89xT4b/view?usp=sharing
-    # --weight_dir[abs - path - to - the - weights - downloaded - above(should be ended with im2latex_weights_prod /)]
+    # --weight_dir [abs - path - to - the - weights - downloaded - above(should be ended with im2latex_weights_prod /)]
     weight_dir = '/home/paulluh/im2latex_weights_prod/'
 
     # Configuration steps
