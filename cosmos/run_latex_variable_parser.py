2bec965cf2166531fd372360779bae910be4552e
Paul Luh <paul841029@gmail.com>
2019-02-15 09:55:49 -0600

    Update run_latex_variable_parser.py

--- cosmos/run_latex_variable_parser.py
+++ cosmos/run_latex_variable_parser.py
@@ -1,6 +1,6 @@
-from latex_parser.variable_extractor import get_latex_variables_api
+from latex_parser.variable_extractor import get_latex_variables
 
 if __name__ == '__main__':
     # Setup
     # 1. install dependencies in latex_parser/requirements.txt
-    print(get_latex_variables_api('\\frac{-b\pm\sqrt{b^2-4ac}}{2a}'))
+    print(get_latex_variables('\\frac{-b\pm\sqrt{b^2-4ac}}{2a}'))
