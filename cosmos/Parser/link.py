758061471221c03c5a400bcc747bcee9e370c3d3
Paulluh <paul841029@gmail.com>
2019-02-15 09:40:29 -0600

    tweak

--- cosmos/Parser/link.py
+++ cosmos/Parser/link.py
@@ -1,19 +1,20 @@
+import loguru
+from collections import defaultdict
+from itertools import chain
 import argparse
 from fonduer import Meta
 from fonduer.parser.models import Document, Sentence
 import json
-import sys, os
+import sys
+import os
 sys.path.append(os.path.dirname(__file__))
-from itertools import chain
-from collections import defaultdict
-import loguru
 
 
 def link(words_location, db_connect_str, ignored_files=[]):
     session = Meta.init(db_connect_str).Session()
 
     def get_word_bag(html_source):
-        with open(words_location + html_source + '.html.json') as words:
+        with open(words_location + html_source + '.html.json', encoding='utf-8') as words:
             return json.load(words)
 
     def get_all_documents():
@@ -31,11 +32,13 @@ def link(words_location, db_connect_str, ignored_files=[]):
         word_bag = get_word_bag(doc.name)
         sentences = get_all_sentence_from_a_doc(doc.id)
         word_bag_count = 0
-        all_words_from_db = list(chain(*[sent.text.split() for sent in sentences]))
+        all_words_from_db = list(
+            chain(*[sent.text.split() for sent in sentences]))
 
         assert len(all_words_from_db) >= len(word_bag)
-        open('db_words.txt', 'w').write('\n'.join(all_words_from_db))
-        open('json_words.txt', 'w').write('\n'.join(map(lambda x: x['text'], word_bag)))
+        open('db_words.txt', 'w', encoding='utf-8').write('\n'.join(all_words_from_db))
+        open('json_words.txt', 'w',
+             encoding='utf-8').write('\n'.join(map(lambda x: x['text'], word_bag)))
         str_buffer = ''
 
         for sent in sentences:
@@ -48,14 +51,18 @@ def link(words_location, db_connect_str, ignored_files=[]):
 
             def add_to_coordinate_record_list(current_idx_json):
                 current_word_from_bag = word_bag[current_idx_json]
-                coordinates_record['top'].append(current_word_from_bag['line_bbox']['ymin'])
-                coordinates_record['left'].append(current_word_from_bag['word_bbox']['xmin'])
-                coordinates_record['bottom'].append(current_word_from_bag['line_bbox']['ymax'])
-                coordinates_record['right'].append(current_word_from_bag['word_bbox']['xmax'])
-                coordinates_record['page_num'].append(current_word_from_bag['word_bbox']['page_num'])
+                coordinates_record['top'].append(
+                    current_word_from_bag['line_bbox']['ymin'])
+                coordinates_record['left'].append(
+                    current_word_from_bag['word_bbox']['xmin'])
+                coordinates_record['bottom'].append(
+                    current_word_from_bag['line_bbox']['ymax'])
+                coordinates_record['right'].append(
+                    current_word_from_bag['word_bbox']['xmax'])
+                coordinates_record['page_num'].append(
+                    current_word_from_bag['word_bbox']['page_num'])
 
             for word in tokenized_words:
-
                 add_to_coordinate_record_list(word_bag_count)
                 if same(word, word_bag[word_bag_count]['text']):
                     word_bag_count += 1
@@ -90,8 +97,8 @@ def link(words_location, db_connect_str, ignored_files=[]):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--words_location', default='out/words/')
-    parser.add_argument('--database', default='postgres://postgres:password@localhost:5432/cosmos')
-    parser.add_argument('--ignored_files', nargs='+', default=[])
+    parser.add_argument('--words_location', help='location of word coordinate JSON file', default='out/words/')
+    parser.add_argument('--database', help='database connection string', default='postgres://postgres:password@localhost:5432/cosmos')
+    parser.add_argument('--ignored_files', help='files to be ignored', nargs='+', default=[])
     args = parser.parse_args()
     link(args.words_location, args.database, args.ignored_files)
