952635b2b1f3631d9e816cac9ccd77f19b44e268
Ankur Goswami <ankur.goswami12@gmail.com>
2019-02-20 15:59:13 -0600

    Fix imports, remove dead code from run.py

--- cosmos/run.py
+++ cosmos/run.py
@@ -10,17 +10,17 @@ from argparse import ArgumentParser
 import mrcnn.model as modellib
 from config import PageConfig
 from config import ingestion_settings
-from dataset import PageDataset
+from dataset.dataset import PageDataset
 import os
 import subprocess
-from model2xml import model2xml
-from xml2list import xml2list
-from list2html import list2html
+from converters.model2xml import model2xml
+from converters.xml2list import xml2list
+from converters.list2html import list2html
 from tqdm import tqdm
 import shutil
 import preprocess.preprocess as pp
 import postprocess.postprocess as post
-from voc_utils import ICDAR_convert
+from utils.voc_utils import ICDAR_convert
 from connected_components.connected_components import write_proposals
 from proposal_matcher.process import process_doc
 
@@ -38,21 +38,22 @@ parser.add_argument('--debug', help="Ingest html documents and create postgres d
 
 args = parser.parse_args()
 
+# Path variables
 tmp = args.tmp_path
 xml = os.path.join(args.output, "xml")
 html = os.path.join(args.output, "html")
+img_d = os.path.join(tmp, 'images2')
 
+# Define and create required paths
 req_paths = [tmp, f'{tmp}/images', f'{tmp}/images2', f'{tmp}/cc_proposals']
 for path in req_paths:
     if not os.path.exists(path):
         os.makedirs(path)
 
-img_d = os.path.join(tmp, 'images2')
+# Convert a pdf into a set of images
 def preprocess_pdfs(pdf_path):
     subprocess.run(['gs', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4',
                     '-dTextAlphaBits=4', '-r600', f'-sOutputFile="{tmp}/images/{pdf_path}_%d.png"', os.path.join(args.pdfdir, pdf_path)])
-    #subprocess.run(['convert', '-density', '100', '-trim', os.path.join(args.pdfdir, pdf_path), '-quality', '100',
-    #                '-sharpen', '0x1.0', os.path.join('tmp','images', f'{pdf_path}-%04d.png')])
 
 def resize_pngs(img_path):
     path, im = pp.resize_png(os.path.join(f'{tmp}', 'images', img_path))
@@ -69,25 +70,13 @@ def preprocess_pngs(img_f):
 
 def convert_to_html(xml_f):
     xpath = os.path.join(xml, xml_f)
-    print('Begin convert')
-    print(xpath)
     l = xml2list(xpath)
-    print(l)
     list2html(l, f'{xml_f[:-4]}.png', img_d, html)
 
 def match_proposal(proposal_f):
     proposal_f_full = os.path.join(f'{tmp}', proposal_f)
     xml_f = f'{xml}/{proposal_f[:-4]}' + '.xml'
     process_doc(xml_f, proposal_f_full, xml_f)
-#for pdf_f in os.listdir(args.pdfdir):
-#    subprocess.run(['gs', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4',
-#                    '-dTextAlphaBits=4', '-r600', f'-sOutputFile="tmp/images2/{pdf_f}_%d.png"', os.path.join(args.pdfdir, pdf_f)])
-#    subprocess.run(['convert', '-density', '150', '-trim', os.path.join(args.pdfdir, pdf_f), '-quality', '100',
-#                    '-sharpen', '0x1.0', os.path.join('tmp','images', f'{pdf_f}-%04d.png')])
-#for img_f in os.listdir('tmp/images'):
-#    subprocess.run(['convert', '-flatten', os.path.join('tmp', 'images', img_f), os.path.join('tmp', 'images2', img_f)])
-
-#shutil.rmtree('tmp/images')
 
 pool = mp.Pool(processes=args.threads)
 results = [pool.apply_async(preprocess_pdfs, args=(x,)) for x in os.listdir(args.pdfdir)]
@@ -97,15 +86,10 @@ results = [pool.apply_async(resize_pngs, args=(x,)) for x in os.listdir(os.path.
 [r.get() for r in results]
 
 print('Begin writing proposals')
-#[write_proposals(os.path.join('tmp', 'images', x)) for x in os.listdir(os.path.join('tmp', 'images'))]
+
 results = [pool.apply_async(write_proposals, args=(os.path.join(f'{tmp}', 'images', x),), kwds={"output_dir" : tmp}) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
 [r.get() for r in results]
 
-#for img_f in os.listdir('tmp/images2'):
-#    path, im = pp.resize_png(os.path.join('tmp', 'images2', img_f))
-#    im.save(os.path.join('tmp', 'images2', img_f))
-#    pth, padded_img = pp.pad_image(os.path.join('tmp', 'images2', img_f))
-#    padded_img.save(os.path.join('tmp', 'images3', img_f))
 
 print('Begin preprocessing pngs')
 results = [pool.apply_async(preprocess_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
@@ -116,16 +100,6 @@ with open('test.txt', 'w') as wf:
         wf.write(f[:-4] + '\n')
 
 shutil.move('test.txt', f'{tmp}/test.txt')
-
-
-#class InferenceConfig(Config):
-#    NAME = "pages"
-#    BACKBONE = "resnet50"
-#    GPU_COUNT = 1
-#    IMAGE_MAX_DIM = 1920
-#    RPN_ANCHOR_SCALES = (32,64, 256, 512,1024)
-#    NUM_CLASSES = 5
-#    IMAGES_PER_GPU = 1
 
 class InferenceConfig(Config):
     NAME = "pages_uncollapsed"
@@ -141,11 +115,9 @@ config = PageConfig()
 model = modellib.MaskRCNN(mode="inference",
                           config=inference_config,
                           model_dir=args.weightsdir)
-#model_path = model.find_last()
-#print("Loading weights from ", model_path)
+
 model.load_weights(args.weights, by_name=True)
 data_test = PageDataset('test', f'{tmp}', 0, nomask=True)
-#data_test.load_page(classes=['Figure', 'Table', 'Equation', 'Body Text'])
 data_test.load_page(classes=list(ICDAR_convert.keys()))
 data_test.prepare()
 image_ids = data_test.image_ids
@@ -173,11 +145,6 @@ if not os.path.exists(html):
 print('Begin converting to html')
 results = [pool.apply_async(convert_to_html, args=(x,)) for x in os.listdir(xml)]
 [r.get() for r in results]
-print('End converting to html')
-#for xml_f in os.listdir(xml):
-#    xpath = os.path.join(xml, xml_f)
-#    l = xml2list(xpath)
-#    list2html(l, f'{xml_f[:-4]}.png', f'{tmp}/images', html)
 
 # postprocess
 tmp_html = html.replace('html', 'html_tmp')
