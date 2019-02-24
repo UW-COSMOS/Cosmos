b883e3dc01c24f15c5e0fd26fb75027fb621e47b
Josh McGrath <joshuawmcgrath@gmail.com>
2019-02-24 16:24:55 -0600

    dead code elimination

--- cosmos/torch_model/model/layers/featurization.py
+++ cosmos/torch_model/model/layers/featurization.py
@@ -3,10 +3,8 @@ Class to build and manage featurization backbones
 Author: Josh McGrath
 """
 from torch import nn
-from model.roi.roi_align import ROIAlign
-from model.rpn.rpn import RPN
+
 from model.connected_components.cc_layer import CCLayer
-from model.proposal.proposal_layer import ProposalLayer
 from model.backbone.backbone import get_backbone
 class Featurizer(nn.Module):
 
@@ -25,18 +23,6 @@ class Featurizer(nn.Module):
         self.RPN_history = []
         if self.method == "CONNECTED_COMPONENTS":
             self.cc = CCLayer(cfg)
-        if self.method == "RPN":
-            self.RPN = RPN(cfg.BACKBONE_DEPTH, cfg.RPN.DIM, cfg.RATIOS, cfg.SCALES)
-            self.ROI_Align = ROIAlign(cfg.ROI_ALIGN.OUTPUT_SIZE,
-                                      cfg.ROI_ALIGN.SPATIAL_SCALE,
-                                      cfg.ROI_ALIGN.SAMPLING_RATIO)
-            self.proposal_layer = ProposalLayer(cfg.RATIOS,
-                                                cfg.SCALES,
-                                                cfg.IMAGE_SIZE,
-                                                cfg.NMS_PRE,
-                                                cfg.NMS_POST,
-                                                cfg.MIN_SIZE,
-                                                cfg.NMS_THRESHOLD)
 
     def forward(self, *input,**kwargs):
         """
@@ -51,13 +37,7 @@ class Featurizer(nn.Module):
         return windows
 
     def _forward_RPN(self, imgs, device):
-        feature_map = self.backbone(imgs)
-        cls_branch_preds, cls_branch_scores, bbox_branch_preds = self.RPN(feature_map)
-        if self.training:
-            self.RPN_history = (cls_branch_preds, cls_branch_scores, bbox_branch_preds)
-        proposals = self.proposal_layer(cls_branch_scores, bbox_branch_preds, device)
-        windows = self.ROI_Align(proposals)
-        return windows, proposals
+        raise NotImplementedError()
 
     def _forward_CC(self, img_windows,device, proposals=None):
         windows = self.backbone(img_windows)
