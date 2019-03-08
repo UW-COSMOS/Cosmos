a509a461934e95bc05ee4ee674d927cd3539e7e1
Josh McGrath <joshuawmcgrath@gmail.com>
2019-03-08 12:20:11 -0600

    coco and voc eval code

--- cosmos/evaluate/evaluate_iccv.py
+++ cosmos/evaluate/evaluate_iccv.py
@@ -1,29 +1,8 @@
 import pandas as pd
 import numpy as np
-from converters.xml2list import xml2list
 from os import listdir
 from os.path import splitext, join
-
-def ingest_file(path):
-    """
-    Ingest an XML file to a dataframe
-    :param path: path to XML file
-    :return: dataframe of [id, label, x0,y0,x1,y1]
-    """
-    lst = xml2list(path)
-    labels = [item[0] for item in lst]
-    coords = [item[1] for item in lst]
-    x0 = [coord[0] for coord in coords]
-    y0 = [coord[1] for coord in coords]
-    x1 = [coord[2] for coord in coords]
-    y1 = [coord[3] for coord in coords]
-    return pd.DataFrame({
-            "label": labels,
-            "x0": x0,
-            "x1": x1,
-            "y0": y0,
-            "y1": y1
-        })
+from .ingestion import ingest_file
 
 def get_ious(pred_df, box):
     X = 0
