022e12fe6df4ea943cb56ecea40de508616fa5dd
Josh McGrath <joshuawmcgrath@gmail.com>
2019-03-12 13:03:17 -0500

    neighbors table

--- cosmos/torch_model/train/data_layer/sql_types.py
+++ cosmos/torch_model/train/data_layer/sql_types.py
@@ -3,7 +3,7 @@ Place holder for Sql Alchemy mappings
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String, LargeBinary, Sequence, PrimaryKeyConstraint
+from sqlalchemy import Column, Integer, String, LargeBinary, Sequence, PrimaryKeyConstraint, ForeignKeyConstraint, CheckConstraint
 from sqlalchemy.ext.declarative import declarative_base
 import pickle
 Base = declarative_base()
@@ -19,7 +19,7 @@ class Example(Base):
     _window = Column("window",LargeBinary)
     _bbox = Column("bbox", LargeBinary)
     label = Column(String)
-    __table__args = (PrimaryKeyConstraint("doc_id", "page_id","object_id",name="example_pk"),)
+    __table_args__ = (PrimaryKeyConstraint("doc_id", "page_id","object_id",name="example_pk"),)
 
     @property
     def window(self):
@@ -29,6 +29,13 @@ class Example(Base):
     def window(self, window):
         self._window = pickle.dumps(window)
 
+    @property
+    def bbox(self):
+        return pickle.loads(self._window)
+
+    @bbox.setter
+    def bbox(self, bbox):
+        self._bbox = pickle.dumps(bbox)
 
     def __repr__(self):
         return f"Example(id={self.id}, window={self.window.shape}, label=self.label)"
@@ -38,7 +45,19 @@ class Neighbor(Base):
     """
     ORM mapping for Neighbors
     """
-    pass
+    __tablename__ = "neighborhoods"
+    center_doc_id = Column(Integer)
+    center_page_id = Column(Integer)
+    center_object_id = Column(Integer)
+    neighbor_doc_id = Column(Integer)
+    neighbor_page_id = Column(Integer) 
+    neighbor_object_id = Column(Integer)
+    __table_args__ = (ForeignKeyConstraint(["center_doc_id", "center_page_id", "center_object_id"], ["examples.doc_id", "examples.page_id", "examples.object_id"]),
+                        ForeignKeyConstraint(["neighbor_doc_id", "neighbor_page_id", "neighbor_object_id"], ["examples.doc_id", "examples.page_id", "examples.object_id"]),
+                        CheckConstraint("center_page_id = neighbor_page_id", name="same_page_check"),
+                        CheckConstraint("center_doc_id = neighbor_doc_id", name="same_doc_check"),
+                        CheckConstraint("center_object_id != neighbor_object_id", name="not_same_object_check")
+            )
 
 
 class ImageDB:
