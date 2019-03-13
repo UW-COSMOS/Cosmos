e7aaf06ea3acece2ad277f1b5a9fd1d796fb85ad
Josh McGrath <joshuawmcgrath@gmail.com>
2019-03-13 15:31:00 -0500

    model integration init test passing

--- cosmos/torch_model/model/attention/embedder.py
+++ cosmos/torch_model/model/attention/embedder.py
@@ -11,6 +11,8 @@ from torch.nn.functional import relu
 class ImageEmbedder(nn.Module):
     def __init__(self,conv_size,conv_depth, intermediate_d, out_d):
         super(ImageEmbedder, self).__init__()       
+        print("super called")
+        print(f"going to build a {conv_size*conv_size*conv_depth} by {intermediate_d} matrix of weights")
         self.FC = nn.Linear(conv_size*conv_size*conv_depth, intermediate_d)
         self.out = nn.Linear(intermediate_d, out_d)
 
