a9af05d1de349368c70b41f2395bc88e68684a9d
Ankur Goswami <ankur.goswami12@gmail.com>
2019-03-28 18:19:47 -0500

    Docs updates

--- cosmos/run.py
+++ cosmos/run.py
@@ -33,152 +33,153 @@ from config import ingestion_settings
 
 # PDF directory path
 
-parser = ArgumentParser(description="Run the classifier")
-parser.add_argument("pdfdir", type=str, help="Path to directory of PDFs")
-parser.add_argument('-d', "--device", default='cpu', type=str, help="Path to weights dir")
-parser.add_argument('-w', "--weights", type=str, help='Path to weights file', required=True)
-parser.add_argument('-t', "--threads", default=160, type=int, help="Number of threads to use")
-parser.add_argument('-n', "--noingest", help="Ingest html documents and create postgres database", action='store_true')
-parser.add_argument('-k', "--keep_pages", help="Keep the page-level PNGs", action='store_true')
-parser.add_argument('-o', "--output", default='./', help="Output directory")
-parser.add_argument('-p', "--tmp_path", default='tmp', help="Path to directory for temporary files")
-parser.add_argument('--debug', help="Ingest html documents and create postgres database", action='store_true')
-
-args = parser.parse_args()
-
-# Path variables
-model_config = "model_config.yaml"
-weights = args.weights
-device = args.device
-tmp = args.tmp_path
-xml = os.path.join(args.output, "xml")
-html = os.path.join(args.output, "html")
-img_d = os.path.join(tmp, 'images2')
-
-# Define and create required paths
-req_paths = [tmp, f'{tmp}/images', f'{tmp}/images2', f'{tmp}/cc_proposals', xml]
-for path in req_paths:
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-# do pdfminer for all pdfs and preserve in mem
-unicodes = {}
-for pdf_name in os.listdir(args.pdfdir):
-    print(os.path.join(args.pdfdir,pdf_name))
-    df, limit = parse_pdf(os.path.join(args.pdfdir,pdf_name))
-    unicodes[pdf_name] = (df, limit)
-
-# Convert a pdf into a set of images
-def preprocess_pdfs(pdf_path):
-    subprocess.run(['gs', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4',
-                    '-dTextAlphaBits=4', '-r600', f'-sOutputFile="{tmp}/images/{pdf_path}_%d.png"', os.path.join(args.pdfdir, pdf_path)])
-
-def resize_pngs(img_path):
-    path, im = pp.resize_png(os.path.join(f'{tmp}', 'images', img_path))
-    if path is not None:
-        im.save(os.path.join(f'{tmp}', 'images', img_path))
-
-def flatten_png(img_f):
-    subprocess.run(['convert', '-flatten', os.path.join(f'{tmp}', 'images', img_f), os.path.join(f'{tmp}', 'images', img_f)])
-
-def preprocess_pngs(img_f):
-    pth, padded_img = pp.pad_image(os.path.join(f'{tmp}', 'images', img_f))
-    if pth is not None:
-        padded_img.save(os.path.join(img_d, img_f))
-
-FILE_NAME = re.compile("(.*\.pdf)_([0-9]+)\.png")
-
-def convert_to_html(xml_f):
-    xpath = os.path.join(xml, xml_f)
-    l = xml2list(xpath)
-    pdf_name = FILE_NAME.search(f'{xml_f[:-4]}.png').group(1)
-    list2html(l, f'{xml_f[:-4]}.png', os.path.join(f'{tmp}', 'images'), html, unicodes[pdf_name])
-
-def update_xmls(html_f):
-    hpath = os.path.join(html, html_f)
-    htmlfile2xml(hpath, xml)
-
-pool = mp.Pool(processes=args.threads)
-results = [pool.apply_async(preprocess_pdfs, args=(x,)) for x in os.listdir(args.pdfdir)]
-[r.get() for r in results]
-
-results = [pool.apply_async(resize_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
-[r.get() for r in results]
-
-print('Begin writing proposals')
-
-results = [pool.apply_async(write_proposals, args=(os.path.join(f'{tmp}', 'images', x),), kwds={"output_dir" : os.path.join(tmp,"cc_proposals")}) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
-[r.get() for r in results]
-
-
-print('Begin preprocessing pngs')
-results = [pool.apply_async(preprocess_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
-[r.get() for r in results]
-
-with open('test.txt', 'w') as wf:
-    for f in os.listdir(f'{tmp}/images'):
-        wf.write(f[:-4] + '\n')
-
-shutil.move('test.txt', f'{tmp}/test.txt')
-
-run_inference(f'{tmp}/images', f'{tmp}/cc_proposals', model_config, weights, xml, device)
-
-if not os.path.exists(html):
-    os.makedirs(html)
-    os.makedirs(os.path.join(html, 'img'))
-    os.makedirs(os.path.join(html, 'latex'))
-
-print('Begin converting to html')
-results = [pool.apply_async(convert_to_html, args=(x,)) for x in os.listdir(xml)]
-[r.get() for r in results]
-
-# postprocess
-tmp_html = html.replace('html', 'html_tmp')
-if not os.path.exists(tmp_html):
-    os.makedirs(tmp_html)
-
-print("Running postprocessing")
-post.postprocess(html, tmp_html)
-
-# replace old html with corrected stuff.
-if not args.debug:
-    shutil.move(os.path.join(html, 'img'), tmp_html)
-    shutil.rmtree(html)
-    shutil.move(tmp_html, html)
-
-results = [pool.apply_async(update_xmls, args=(x,)) for x in os.listdir(html) if x != 'img']
-[r.get() for r in results]
-
-# Construct handles multiprocessing within it.
-
-construct(html, 'Figure Caption', 'Figure', os.path.join(args.output, 'figures.csv'), processes=args.threads)
-construct(html, 'Table Caption', 'Table', os.path.join(args.output, 'tables.csv'), processes=args.threads)
-
-# Parse html files to postgres db
-input_folder = ingestion_settings['input_folder']
-print(input_folder)
-
-# intermediate folder location (will be auto-generated)
-merge_folder = ingestion_settings['merge_folder']
-output_html = ingestion_settings['output_html']
-output_words = ingestion_settings['output_words']
-output_equations = ingestion_settings['output_equations']
-
-db_connect_str = ingestion_settings['db_connect_str']
-
-strip_tags = ingestion_settings['strip_tags']
-ignored_file_when_link = ingestion_settings['ignored_file_when_link']
-output_csv = os.path.join(args.output, "output.csv")
-corenlp_fd = '/app/stanford-corenlp-full-2018-10-05'
-
-if not args.noingest:
-    parse_html_to_postgres(input_folder, output_html, merge_folder, output_words, output_equations, db_connect_str, strip_tags, ignored_file_when_link, output_csv, corenlp_fd)
-
-if args.keep_pages:
-    if not os.path.exists(args.output + "/images/"):
-        os.makedirs(args.output + "/images/")
-    for page in glob.glob(f"{tmp}/images/*.png"):
-        shutil.move(page, args.output + "/images/")
-
-if not args.debug:
-    shutil.rmtree(f'{tmp}')
+if __name__ == '__main__':
+    parser = ArgumentParser(description="Run the classifier")
+    parser.add_argument("pdfdir", type=str, help="Path to directory of PDFs")
+    parser.add_argument('-d', "--device", default='cpu', type=str, help="Path to weights dir")
+    parser.add_argument('-w', "--weights", type=str, help='Path to weights file', required=True)
+    parser.add_argument('-t', "--threads", default=160, type=int, help="Number of threads to use")
+    parser.add_argument('-n', "--noingest", help="Ingest html documents and create postgres database", action='store_true')
+    parser.add_argument('-k', "--keep_pages", help="Keep the page-level PNGs", action='store_true')
+    parser.add_argument('-o', "--output", default='./', help="Output directory")
+    parser.add_argument('-p', "--tmp_path", default='tmp', help="Path to directory for temporary files")
+    parser.add_argument('--debug', help="Ingest html documents and create postgres database", action='store_true')
+    
+    args = parser.parse_args()
+    
+    # Path variables
+    model_config = "model_config.yaml"
+    weights = args.weights
+    device = args.device
+    tmp = args.tmp_path
+    xml = os.path.join(args.output, "xml")
+    html = os.path.join(args.output, "html")
+    img_d = os.path.join(tmp, 'images2')
+    
+    # Define and create required paths
+    req_paths = [tmp, f'{tmp}/images', f'{tmp}/images2', f'{tmp}/cc_proposals', xml]
+    for path in req_paths:
+        if not os.path.exists(path):
+            os.makedirs(path)
+    
+    # do pdfminer for all pdfs and preserve in mem
+    unicodes = {}
+    for pdf_name in os.listdir(args.pdfdir):
+        print(os.path.join(args.pdfdir,pdf_name))
+        df, limit = parse_pdf(os.path.join(args.pdfdir,pdf_name))
+        unicodes[pdf_name] = (df, limit)
+    
+    # Convert a pdf into a set of images
+    def preprocess_pdfs(pdf_path):
+        subprocess.run(['gs', '-dBATCH', '-dNOPAUSE', '-sDEVICE=png16m', '-dGraphicsAlphaBits=4',
+                        '-dTextAlphaBits=4', '-r600', f'-sOutputFile="{tmp}/images/{pdf_path}_%d.png"', os.path.join(args.pdfdir, pdf_path)])
+    
+    def resize_pngs(img_path):
+        path, im = pp.resize_png(os.path.join(f'{tmp}', 'images', img_path))
+        if path is not None:
+            im.save(os.path.join(f'{tmp}', 'images', img_path))
+    
+    def flatten_png(img_f):
+        subprocess.run(['convert', '-flatten', os.path.join(f'{tmp}', 'images', img_f), os.path.join(f'{tmp}', 'images', img_f)])
+    
+    def preprocess_pngs(img_f):
+        pth, padded_img = pp.pad_image(os.path.join(f'{tmp}', 'images', img_f))
+        if pth is not None:
+            padded_img.save(os.path.join(img_d, img_f))
+    
+    FILE_NAME = re.compile("(.*\.pdf)_([0-9]+)\.png")
+    
+    def convert_to_html(xml_f):
+        xpath = os.path.join(xml, xml_f)
+        l = xml2list(xpath)
+        pdf_name = FILE_NAME.search(f'{xml_f[:-4]}.png').group(1)
+        list2html(l, f'{xml_f[:-4]}.png', os.path.join(f'{tmp}', 'images'), html, unicodes[pdf_name])
+    
+    def update_xmls(html_f):
+        hpath = os.path.join(html, html_f)
+        htmlfile2xml(hpath, xml)
+    
+    pool = mp.Pool(processes=args.threads)
+    results = [pool.apply_async(preprocess_pdfs, args=(x,)) for x in os.listdir(args.pdfdir)]
+    [r.get() for r in results]
+    
+    results = [pool.apply_async(resize_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
+    [r.get() for r in results]
+    
+    print('Begin writing proposals')
+    
+    results = [pool.apply_async(write_proposals, args=(os.path.join(f'{tmp}', 'images', x),), kwds={"output_dir" : os.path.join(tmp,"cc_proposals")}) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
+    [r.get() for r in results]
+    
+    
+    print('Begin preprocessing pngs')
+    results = [pool.apply_async(preprocess_pngs, args=(x,)) for x in os.listdir(os.path.join(f'{tmp}', 'images'))]
+    [r.get() for r in results]
+    
+    with open('test.txt', 'w') as wf:
+        for f in os.listdir(f'{tmp}/images'):
+            wf.write(f[:-4] + '\n')
+    
+    shutil.move('test.txt', f'{tmp}/test.txt')
+    
+    run_inference(f'{tmp}/images', f'{tmp}/cc_proposals', model_config, weights, xml, device)
+    
+    if not os.path.exists(html):
+        os.makedirs(html)
+        os.makedirs(os.path.join(html, 'img'))
+        os.makedirs(os.path.join(html, 'latex'))
+    
+    print('Begin converting to html')
+    results = [pool.apply_async(convert_to_html, args=(x,)) for x in os.listdir(xml)]
+    [r.get() for r in results]
+    
+    # postprocess
+    tmp_html = html.replace('html', 'html_tmp')
+    if not os.path.exists(tmp_html):
+        os.makedirs(tmp_html)
+    
+    print("Running postprocessing")
+    post.postprocess(html, tmp_html)
+    
+    # replace old html with corrected stuff.
+    if not args.debug:
+        shutil.move(os.path.join(html, 'img'), tmp_html)
+        shutil.rmtree(html)
+        shutil.move(tmp_html, html)
+    
+    results = [pool.apply_async(update_xmls, args=(x,)) for x in os.listdir(html) if x != 'img']
+    [r.get() for r in results]
+    
+    # Construct handles multiprocessing within it.
+    
+    construct(html, 'Figure Caption', 'Figure', os.path.join(args.output, 'figures.csv'), processes=args.threads)
+    construct(html, 'Table Caption', 'Table', os.path.join(args.output, 'tables.csv'), processes=args.threads)
+    
+    # Parse html files to postgres db
+    input_folder = ingestion_settings['input_folder']
+    print(input_folder)
+    
+    # intermediate folder location (will be auto-generated)
+    merge_folder = ingestion_settings['merge_folder']
+    output_html = ingestion_settings['output_html']
+    output_words = ingestion_settings['output_words']
+    output_equations = ingestion_settings['output_equations']
+    
+    db_connect_str = ingestion_settings['db_connect_str']
+    
+    strip_tags = ingestion_settings['strip_tags']
+    ignored_file_when_link = ingestion_settings['ignored_file_when_link']
+    output_csv = os.path.join(args.output, "output.csv")
+    corenlp_fd = '/app/stanford-corenlp-full-2018-10-05'
+    
+    if not args.noingest:
+        parse_html_to_postgres(input_folder, output_html, merge_folder, output_words, output_equations, db_connect_str, strip_tags, ignored_file_when_link, output_csv, corenlp_fd)
+    
+    if args.keep_pages:
+        if not os.path.exists(args.output + "/images/"):
+            os.makedirs(args.output + "/images/")
+        for page in glob.glob(f"{tmp}/images/*.png"):
+            shutil.move(page, args.output + "/images/")
+    
+    if not args.debug:
+        shutil.rmtree(f'{tmp}')
