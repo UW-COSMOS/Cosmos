c4e79c689587b4dd8bacb585ba86f39f1b6b1aea
Ankur Goswami <ankur.goswami12@gmail.com>
2019-04-01 16:21:55 -0500

    Make apply post processing synchronous

--- cosmos/apply_post_processing.py
+++ cosmos/apply_post_processing.py
@@ -12,6 +12,7 @@ import os
 
 def convert_to_html(xpath, img_dir):
     l = xml2list(xpath)
+    print(l)
     print(f'{os.path.basename(xpath)[:-4]}.png')
     list2html(l, f'{os.path.basename(xpath)[:-4]}.png', img_dir, 'html2')
 
@@ -23,12 +24,18 @@ def update_xmls(hpath, output_dir):
 @click.argument('img_dir')
 @click.argument('output_dir')
 def run_xml_postprocessing(xml_dir, img_dir, output_dir):
-    pool = mp.Pool(processes=140)
-    results = [pool.apply_async(convert_to_html, args=(os.path.join(xml_dir, x), img_dir)) for x in os.listdir(xml_dir)]
-    [r.get() for r in results]
+    #pool = mp.Pool(processes=140)
+    #results = [pool.apply_async(convert_to_html, args=(os.path.join(xml_dir, x), img_dir)) for x in os.listdir(xml_dir)]
+    #[r.get() for r in results]
+    for x in os.listdir(xml_dir):
+        convert_to_html(os.path.join(xml_dir, x), img_dir)
     post.postprocess('html2', 'html3')
-    results = [pool.apply_async(update_xmls, args=(os.path.join('html3', x), output_dir)) for x in os.listdir('html3') if x != 'img']
-    [r.get() for r in results]
+    #results = [pool.apply_async(update_xmls, args=(os.path.join('html3', x), output_dir)) for x in os.listdir('html3') if x != 'img']
+    #[r.get() for r in results]
+    for x in os.listdir('html3'):
+        if x == 'img':
+            continue
+        update_xmls(os.path.join('html3', x), output_dir)
 
 
 if __name__ == '__main__':
