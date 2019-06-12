271a2b23dcab436107a8c3fdad7bcc7bd60c9a52
Ian Ross <ian.ackerman.ross@gmail.com>
2019-06-12 16:30:25 -0500

    wip -- read page bytes from that collection. Currently works, but swallows logging

--- services/detection/src/detect.py
+++ services/detection/src/detect.py
@@ -6,6 +6,7 @@ import pymongo
 from pymongo import MongoClient
 import os
 import logging
+import datetime
 logging.basicConfig(format='%(levelname)s :: %(asctime)s :: %(message)s', level=logging.DEBUG)
 import time
 import io
@@ -34,16 +35,34 @@ def detect() -> None:
     except pymongo.errors.PyMongoError as err:
         logging.error("Error in pymongo:")
         logging.error(err)
-            
+
     end_time = time.time()
     logging.info(f'Exiting proposal generation. Time up: {end_time - start_time}')
 
+def get_page_bytes(client, pdf_id, page_num):
+    """
+    TODO: Docstring for get_page_bytes.
+
+    Args:
+        pdf_id (TODO): TODO
+        page_no (TODO): TODO
+
+    Returns: TODO
+
+    """
+    db = client.pdfs
+    pages = db.pages
+    result = pages.find_one({"pdf_id" : pdf_id, "page_num" : page_num})
+    bytesio = io.BytesIO(result['resize_bytes'])
+    return bytesio
+
 def process_doc(full: Mapping[T, T], db_insert_fn: Callable[[Mapping[T, T], T], None], client: T, config_pth: str = 'model_config.yaml', weights_pth: str = 'model_weights.pth') -> Mapping[T, T]:
     logging.info('Document found and added to queue')
+
     page_data = full['page_data']
+    # loop over pages once to prep them for inference
     for page in page_data:
-        bstring = base64.decodebytes(page['resize_bytes']['$binary'].encode())
-        bytesio = io.BytesIO(bstring)
+        bytesio = get_page_bytes(client, full["_id"], page["page_num"])
         img = pad_image(bytesio)
         padded_bytes_stream = io.BytesIO()
         img.save(padded_bytes_stream, format='PNG')
@@ -51,6 +70,7 @@ def process_doc(full: Mapping[T, T], db_insert_fn: Callable[[Mapping[T, T], T], 
         page['padded_bytes'] = padded_bytes
     print('test running inference')
     detected_objs = run_inference(page_data, config_pth, weights_pth, os.environ["DEVICE"], pdf_name=full['pdf_name'])
+    # loop over pages again to map detected objects back into the pages to be written.
     for page in page_data:
         page_num = page['page_num']
         page_num = str(page_num)
@@ -60,6 +80,7 @@ def process_doc(full: Mapping[T, T], db_insert_fn: Callable[[Mapping[T, T], T], 
             continue
         objs = detected_objs[page_num]
         page['detected_objs'] = objs
+        del page["padded_bytes"]
 
     full['event_stream'].append('detection')
     db_insert_fn(full, client)
