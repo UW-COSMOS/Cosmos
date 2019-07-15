c97d8f1698a908d443a33f17b454ebc19f6970d5
Ankur Goswami <ankur.goswami12@gmail.com>
2019-07-15 11:03:02 -0500

    Remove OCR df in response object for search

--- services/search_backend/src/app.py
+++ services/search_backend/src/app.py
@@ -10,13 +10,16 @@ import os
 import logging
 logging.basicConfig(format='%(levelname)s :: %(asctime)s :: %(message)s', level=logging.DEBUG)
 from bson import json_util
+import base64
+import json
+from flask import jsonify
 
 class CustomJSONEncoder(JSONEncoder):
     def default(self, obj): return json_util.default(obj)
 
 
 app = Flask(__name__)
-app.json_encoder = CustomJSONEncoder
+#app.json_encoder = CustomJSONEncoder
 
 @app.route('/search')
 def search():
@@ -26,8 +29,13 @@ def search():
         obj_type = request.args.get('type', '')
         query = request.args.get('q', '')
         results = list(db.ocr_objs.find().limit(20))
+        for result in results:
+            result['_id'] = str(result['_id'])
+            encoded = base64.encodebytes(result['bytes'])
+            result['bytes'] = encoded.decode('ascii')
+            del result['page_ocr_df']
         results_obj = {'results': results}
-        return results_obj
+        return jsonify(results_obj) 
     except TypeError:
         abort(400)
     
