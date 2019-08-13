2995e2a291b6be40720574aee9a582b1cd427b02
abhat6 <abhat6@cosmos0004.chtc.wisc.edu>
2019-08-13 14:56:22 -0500

    Integrating Search backend and questions answering backend

--- services/question_answering_backend/src/app.py
+++ services/question_answering_backend/src/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, abort
 from infer import infer_qa
+import logging
+logging.basicConfig(format='%(levelname)s :: %(asctime)s :: %(message)s', level=logging.DEBUG)
 
 app = Flask(__name__)
 @app.route('/query')
 def query():
+    logging.debug("Inside query function")
     try:
         query = request.args.get('query', '')
-	candidate = request.args.get('candidate','')
-	return infer_qa(query, candidate)
+        candidate = request.args.get('candidate','')
+        logging.debug(query, candidate)	
+        answer = infer_qa(query, candidate)
+        return answer
     except:
         abort(400)
