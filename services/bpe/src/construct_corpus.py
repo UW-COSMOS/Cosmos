fec715c7bd60fba492e43ad93eb98c241d85e1ff
Ankur Goswami <ankur.goswami12@gmail.com>
2019-08-20 16:24:06 -0500

    Add corpus construction

---
+++ services/bpe/src/construct_corpus.py
@@ -0,0 +1,39 @@
+"""
+Create a corpus from all the text objects
+"""
+import pymongo
+from pymongo import MongoClient
+import os
+import logging
+logging.basicConfig(format='%(levelname)s :: %(asctime)s :: %(message)s', level=logging.DEBUG)
+import time
+import click
+
+
+def create_corpus():
+    logging.info('Starting corpus construction')
+    start_time = time.time()
+    client = MongoClient(os.environ["DBCONNECT"])
+    logging.info(f'Connected to client: {client}.')
+    db = client.pdfs
+    with open('corpus.txt', 'w') as wf:
+        for doc in db.partialSections.find(no_cursor_timeout=True):
+            content = doc['content']
+            wf.write(f'{content}\n')
+
+        for doc in db.ocr_objs.find({'class': {'$in': ['Abstract', 'Figure Caption', 'Table Caption']}}, no_cursor_timeout=True):
+            content = doc['content']
+            wf.write(f'{content}\n')
+
+
+
+
+@click.command()
+def run():
+    create_corpus()
+
+if __name__ == '__main__':
+    run()
+
+
+
