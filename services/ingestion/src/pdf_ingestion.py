840f92e3d69ed3f09495ac090a87ba85de3aace8
Ian Ross <iross@cs.wisc.edu>
2019-11-18 09:39:57 -0600

    Catch edgecase where a page creates a _huge_ image that PIL can't handle

--- services/ingestion/src/pdf_ingestion.py
+++ services/ingestion/src/pdf_ingestion.py
@@ -33,7 +33,7 @@ def ingest_pdf(pdf_path, pdf_dir, db_insert_fn, db_insert_pages_fn, subprocess_f
         pdf_obj = {}
         logs = []
         pdf_obj, err = load_pdf_metadata(pdf_path, pdf_obj)
-        if not err: 
+        if not err:
             logs.append(err)
         pdf_name = pdf_obj['pdf_name']
         pdf_path = os.path.abspath("%s/%s" % (pdf_dir, pdf_name))
@@ -44,6 +44,8 @@ def ingest_pdf(pdf_path, pdf_dir, db_insert_fn, db_insert_pages_fn, subprocess_f
         subprocess_fn(pdf_path, img_tmp)
         pages = []
         pages = load_page_data(img_tmp, pdf_obj)
+        if pages == []:
+            return [f"{pdf_path} made a impossibly large image -- skipping."]
         try:
             pdf_logs = db_insert_fn(pdf_obj)
             pages_logs = db_insert_pages_fn(pages)
@@ -147,7 +149,11 @@ def load_page_data(img_dir, current_obj):
         logging.info(f)
         page_obj = {}
         page_num = int(os.path.basename(f))
-        img = Image.open(f)
+        try:
+            img = Image.open(f)
+        except Image.DecompressionBombError:
+            logging.warning(f"Couldn't open image {f} - image too large.")
+            return []
         width, height = img.size
         with open(f, 'rb') as bimage:
             bstring = bimage.read()
