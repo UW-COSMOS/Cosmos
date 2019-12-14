37cc40115e1b66bb80922e29a012fc2505118d88
Ankur Goswami <ankur.goswami12@gmail.com>
2019-12-14 16:42:39 -0600

    Delete run_stack.sh

--- services/run_stack.sh
+++
@@ -1,5 +0,0 @@
-export MYSQL_ROOT_PASSWORD=secret123
-export MYSQL_DATABASE=cosmos
-export MYSQL_USER=cosmos
-export MYSQL_PASSWORD=constellation
-docker stack deploy -c docker-compose-database.yml cosmos
