fb3737468625e45789f9b824d450dff6ffc905e7
Ankur Goswami <ankur.goswami12@gmail.com>
2020-01-24 15:15:31 -0600

    Deployment changes

--- services/deploy.sh
+++ services/deploy.sh
@@ -3,4 +3,6 @@ export MYSQL_USER=
 export MYSQL_DATABASE=
 export MYSQL_PASSWORD=
 export DATABASE_DIR=
+export UID=$(id -u)
+export GID=$(id -g)
 docker stack deploy cosmos --compose-file docker-compose-database.yml
