60290e040860f5759920c12f8d07c0b4807d4713
Ankur Goswami <ankur.goswami12@gmail.com>
2020-03-26 15:57:05 -0500

    Add iteration over directory

--- cosmos/scripts/read_dir_and_request.py
+++ cosmos/scripts/read_dir_and_request.py
@@ -8,6 +8,7 @@ import base64
 import logging
 import time
 from tqdm import tqdm
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -25,51 +26,61 @@ def run_request(payload):
 def run(directory):
     did = str(uuid.uuid4())
     logger.info(f'Dataset id generated: {did}')
-    filenames = glob.glob(os.path.join(directory, '*.pdf'))
-    if len(filenames) == 0:
-        logger.error('Empty input directory')
-        return None, None
-    fsizes = [os.stat(f).st_size for f in filenames]
-    fz = zip(filenames, fsizes)
-    srt = sorted(fz, key=lambda x: x[1], reverse=True)
-    filenames, _ = zip(*srt)
-    dids = [did] * len(filenames)
-    zipped = list(zip(filenames, dids))
-    logger.info('Submitting jobs')
-    with ThreadPoolExecutor(max_workers=32) as pool:
-        resps = list(tqdm(pool.map(run_request, zipped), total=len(zipped)))
-    logger.info('Finished submitting jobs')
-    successful_resps = [r for r in resps if r.status_code == 200]
-    logger.info(f'There were {len(resps) - len(successful_resps)} failed job submissions')
-    pages_dict = {}
-    with tqdm(total=len(successful_resps)) as pbar:
-        done_count = 0
-        error_count = 0
-        prev_done_count = 0
-        while done_count < len(successful_resps):
-            done_count = 0
-            error_count = 0
-            for resp in successful_resps:
-                obj = resp.json()
-                tid = obj['data']['task_id']
-                url = f'http://ingestion:8000/status/{tid}'
-                result = requests.get(url)
-                if result.status_code == 200:
-                    obj = result.json()
-                    status = obj['status']
-                    if status == 'SUCCESS':
-                        done_count += 1
-                    elif status == 'FAILURE':
-                        done_count += 1
-                        error_count += 1
-                else:
-                    error_count += 1
-                    done_count += 1
-            if prev_done_count < done_count:
-                pbar.update(done_count - prev_done_count)
-                prev_done_count = done_count
-            time.sleep(10)
-    logger.info(f'Done ingesting. There were {error_count} failures')
+    d = directory
+    directories = [os.path.join(directory, o) for o in os.listdir(directory) if os.path.isdir(os.path.join(directory,o))]
+    processed_subdirs = []
+    for ind, subdir in enumerate(directories):
+        if ind == 4:
+            break
+        processed_subdirs.append(subdir)
+        print(subdir)
+        filenames = glob.glob(os.path.join(subdir, '*.pdf'))
+        if len(filenames) == 0:
+            logger.error('Empty input directory')
+            return None, None
+        fsizes = [os.stat(f).st_size for f in filenames]
+        fz = zip(filenames, fsizes)
+        srt = sorted(fz, key=lambda x: x[1], reverse=True)
+        filenames, _ = zip(*srt)
+        dids = [did] * len(filenames)
+        zipped = list(zip(filenames, dids))
+        logger.info('Submitting jobs')
+        with ThreadPoolExecutor(max_workers=32) as pool:
+            resps = list(tqdm(pool.map(run_request, zipped), total=len(zipped)))
+        logger.info('Finished submitting jobs')
+        successful_resps = [r for r in resps if r.status_code == 200]
+        logger.info(f'There were {len(resps) - len(successful_resps)} failed job submissions')
+        time.sleep(2400)
+    print(processed_subdirs)
+    #pages_dict = {}
+    #with tqdm(total=len(successful_resps)) as pbar:
+    #    done_count = 0
+    #    error_count = 0
+    #    prev_done_count = 0
+    #    while done_count < len(successful_resps):
+    #        done_count = 0
+    #        error_count = 0
+    #        for resp in successful_resps:
+    #            obj = resp.json()
+    #            tid = obj['data']['task_id']
+    #            url = f'http://ingestion:8000/status/{tid}'
+    #            result = requests.get(url)
+    #            if result.status_code == 200:
+    #                obj = result.json()
+    #                status = obj['status']
+    #                if status == 'SUCCESS':
+    #                    done_count += 1
+    #                elif status == 'FAILURE':
+    #                    done_count += 1
+    #                    error_count += 1
+    #            else:
+    #                error_count += 1
+    #                done_count += 1
+    #        if prev_done_count < done_count:
+    #            pbar.update(done_count - prev_done_count)
+    #            prev_done_count = done_count
+    #        time.sleep(10)
+    #logger.info(f'Done ingesting. There were {error_count} failures')
     #tids = []
     #for resp in successful_resps:
     #    obj = resp.json()
