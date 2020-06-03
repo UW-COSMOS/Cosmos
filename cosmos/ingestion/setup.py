8b0455f928e65bcceb2b2ebc4d1f669255a814aa
Ankur Goswami <ankur.goswami12@gmail.com>
2020-06-02 19:34:20 -0500

    Update setup and upload to pypi

--- cosmos/ingestion/setup.py
+++ cosmos/ingestion/setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 setup(
-    name="ingestion",
+    name="cosmos-ingest",
     version="0.1",
+    author="Ankur Goswami",
+    author_email="ankur.goswami12@gmail.com",
+    description="The COSMOS pdf ingestion package",
+    url="https://github.com/UW-COSMOS/Cosmos",
     packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
 
--- examples/README.md
+++ examples/README.md
@@ -1,6 +1,21 @@
 # Examples
 
+## Setup
+
+To run the examples, first initialize a Python 3.6 environment. For example using miniconda:
+
+```bash
+conda create -n cosmos python=3.6
+conda activate cosmos
+```
+
+Then install the requirements in this directory via pip
+
+```bash
+pip install -r requirements.txt
+```
+
 ## Barebones Detection Pipeline
 
-An example of a barebones detection pipeline can be found in detection.ipynb
+An example of the detection pipeline up to the deep learning model's output can be found in detection.ipynb.
 
