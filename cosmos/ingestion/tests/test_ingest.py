b65a8d20a22639af9e8a0127dc97835c3ab2ef83
Ankur Goswami <ankur.goswami12@gmail.com>
2020-07-13 11:39:13 -0500

    Remove fortran code files. Move test docs to the tests folder.

--- cosmos/ingestion/tests/test_ingest.py
+++ cosmos/ingestion/tests/test_ingest.py
@@ -2,4 +2,4 @@ from ingest.ingest import Ingest
 
 if __name__ == '__main__':
     ingest = Ingest('tcp://localhost:8786', tmp_dir='tmp', use_semantic_detection=True)
-    ingest.ingest('c', 'contracts', 'contracts.parquet', remove_watermark=False, visualize_proposals=False)
+    ingest.ingest('/ssd/ankur/c2', 'contracts', 'contracts.parquet', remove_watermark=False, visualize_proposals=False)
