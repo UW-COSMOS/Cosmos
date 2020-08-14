4a6047a5d7e1b89fe68941cd944e7ae9da13ce2a
Ankur Goswami <ankur.goswami12@gmail.com>
2020-08-14 14:55:09 -0500

    Search and QA script

--- cosmos/extraction/local_server.sh
+++ cosmos/extraction/local_server.sh
@@ -1,4 +1,4 @@
-export QA_MODEL_PATH=../weights/bert_base_squad2
+export QA_MODEL_PATH=/ssd/ankur/Cosmos/deployment/weights/bert_base_squad2
 export QA_DEVICE=cuda
 export OMP_NUM_THREADS=8
 dask-worker tcp://localhost:8786 --nprocs 1 --nthreads 8 --memory-limit 0 --resources "qa=1" --preload extraction.qa_extractor
