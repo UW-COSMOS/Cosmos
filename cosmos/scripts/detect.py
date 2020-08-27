4338c410a60f5f481c9dab757c1bbcaf1bd237d9
Ankur Goswami <ankur.goswami12@gmail.com>
2020-08-27 15:01:31 -0500

    Delete old scripts

--- cosmos/scripts/detect.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import requests
-
-with open('test_detect.pkl', 'rb') as rf:
-    obj = pickle.load(rf)
-
-result = requests.post('http://pt:8004/detect', json=obj)
-print(result.json())
-
