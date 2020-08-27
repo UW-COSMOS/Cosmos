d29895c47fe67c092e37fa11868b97799f50f83a
Ankur Goswami <ankur.goswami12@gmail.com>
2020-08-27 17:04:51 -0500

    Remove anserini searcher

--- cosmos/retrieval/retrieval/index.sh
+++
@@ -1,4 +0,0 @@
-# Usage : /src/index.sh <path_to_dataset>
-./anserini/target/appassembler/bin/IndexCollection -collection JsonCollection \
-    -generator LuceneDocumentGenerator -threads 9 -input $1 \
-    -index $1/lucene-index -storePositions -storeDocvectors -storeRawDocs
