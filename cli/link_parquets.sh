0463b8dc2821e1cceb27346cd753e2dc24e8a34e
Ian Ross <iross@cs.wisc.edu>
2020-12-02 14:44:07 -0600

    fix typo preventing linker from connecting to the scheduler

--- cli/link_parquets.sh
+++ cli/link_parquets.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3.8 -m ingest.process.entity_linking.link --input-path /input --output-path /output/ \
-    --cluster tcp://scheduluer:8786 --dataset-id documents
+    --cluster tcp://scheduler:8786 --dataset-id documents
