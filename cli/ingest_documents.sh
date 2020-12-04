09c057fa8770f9c19a152554e6dee63386ed2bf9
Ian Ross <iross@cs.wisc.edu>
2020-12-04 11:50:22 -0600

    fix typo

--- cli/ingest_documents.sh
+++ cli/ingest_documents.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python3.8 -m ingest.scripts.ingest_documents --use-semantic-detection \
     --use-xgboost-postprocess -a pdfs -a sections -a tables -a figures -a equations \
-    --compute-word-vecs --ngrams 3 \
+    --compute-word-vecs --ngram 3 \
     --no-enrich \
     --input-path /input --output-path /output --dataset-id documents \
     --cluster tcp://scheduler:8786 --tmp-dir /mytmp
