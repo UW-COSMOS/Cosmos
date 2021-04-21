8f779877cbaebfcb0f5baeea3b022f17977307e5
ilmcconnell <GithubTim_e!!1>
2021-04-21 11:37:11 -0500

    set logger level back to ERROR

--- cosmos/ingestion/ingest/ingest.py
+++ cosmos/ingestion/ingest/ingest.py
@@ -38,7 +38,7 @@ logging.getLogger("ingest.process.detection.src.utils.ingest_images").setLevel(l
 logging.getLogger("ingest.process.detection.src.torch_model.train.data_layer.xml_loader").setLevel(logging.ERROR)
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 
 class Ingest:
