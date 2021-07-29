b4d692ff9489c2d39fec16297cbf16d1a7ad0305
ilmcconnell <GithubTim_e!!1>
2021-07-29 10:21:06 -0500

    breakout enrichment code to separate file

--- cosmos/ingestion/ingest/scripts/enrich_parquets.py
+++ cosmos/ingestion/ingest/scripts/enrich_parquets.py
@@ -2,13 +2,25 @@ from ingest.ingest import Ingest
 from pathlib import Path
 
 cluster = 'tcp://localhost:8786'
-tmp_dir = '/hdd/iain/enrichment_testing/tmp'
+# tmp_dir = '/hdd/iain/enrichment_testing/tmp'
+# tmp_dir = '/hdd/iain/enrichment_testing/tmp_100_no_preprints'
+# tmp_dir = '/hdd/iain/enrichment_testing/smoke_test_tmp'
+tmp_dir = '/hdd/iain/enrichment_testing/dolomites_tmp'
+
 use_semantic_detection = True
 use_xgboost_postprocess = True
 use_rules_postprocess = False
 
-dataset_id = 'documents_5Feb'
-path_to_parquet_dirs = '/hdd/iain/enrichment_testing/cosmos_covid_5Feb2021_enriched'
+# dataset_id = 'documents_5Feb'
+# dataset_id = 'no_preprints'
+dataset_id = 'dolomites'
+# path_to_parquet_dirs = '/hdd/iain/enrichment_testing/cosmos_covid_5Feb2021_enriched'
+# parquets of 100 no-preprint covid docs (cosmos output): /hdd/iain/enrichment_testing/100_no_preprints_output
+# copy those over to '/hdd/iain/enrichment_testing/100_noq_preprints/'
+# path_to_parquet_dirs = '/hdd/iain/enrichment_testing/100_no_preprints_enriched'
+# path_to_parquet_dirs = '/hdd/iain/enrichment_testing/smoke_test_one_doc'
+path_to_parquet_dirs = '/hdd/iain/enrichment_testing/dolomites_docs/'
+
 pp_threshold = 0.72
 d_threshold = -10
 spans = 20
@@ -25,4 +37,5 @@ if __name__ == '__main__':
                       dataset_id=dataset_id,
                       pp_threshold=pp_threshold,
                       d_threshold=d_threshold,
-                      spans=spans)
+                      spans=spans,
+                      qa=True)
