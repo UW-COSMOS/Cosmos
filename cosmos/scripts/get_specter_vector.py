b3a858c2f32eba9e9f16886c6a689b3c44ea5dd1
ilmcconnell <GithubTim_e!!1>
2021-10-13 09:33:55 -0500

    break out each step to methods, added mock up demo in main()

--- cosmos/scripts/get_specter_vector.py
+++ cosmos/scripts/get_specter_vector.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from transformers import AutoTokenizer, AutoModel
 from pathlib import Path
+from typing import List, Tuple
 
 
 class SPECTERizer:
@@ -19,28 +20,16 @@ class SPECTERizer:
         else:
             raise NotADirectoryError()
 
-    def create_vectors(self):
-        """load parquet create SPECTER vectors parquet and save to disk"""
-        document_sections_df = pd.read_parquet(self.filepath)
-        specter_vectors = self._vectorize(document_sections_df)
-        specter_vectors.to_parquet(self.filepath.parent / (self.dataset_id+'_specter_vectors.parquet'))
-
-    def _vectorize(self, df:pd.DataFrame) -> pd.DataFrame:
-        """
-        get title and abstract per pdf in parquet return df columns pdf_name and specter vector
-
-        get row title and abstracts, return 768 element SPECTER vector
-        https://arxiv.org/pdf/2004.07180.pdf
-        https://github.com/allenai/specter
-        :param row: pandas DataFrame row - i.e. use df.apply(get_vectors)
-        :return: list[float] a SPECTER vector
-        """
-        # get list of dfs each addressing a single pdf
+    @staticmethod
+    def _convert_to_single_doc_dfs(df:pd.DataFrame) -> List[pd.DataFrame]:
+        """convert df of multiple pdfs to list of dfs each addressing a single pdf"""
         all_pdf_names = list(df.pdf_name.unique())
         single_doc_dfs = []
         for name in all_pdf_names:
             single_doc_dfs.append(df[df['pdf_name'] == name])
+        return single_doc_dfs
 
+    def _extract_title_and_abstract(self, single_doc_dfs: List[pd.DataFrame]) -> Tuple[Tuple[str], Tuple[str]]:
         # get title and abstract where possible from each single pdf df
         title_abs = {}
         for doc in single_doc_dfs:
@@ -56,29 +45,72 @@ class SPECTERizer:
 
             if (title and abstract) or (title or abstract):
                 title_abs[pdf_name] = (title + self.tokenizer.sep_token + abstract)
+        return zip(*title_abs.items())
 
-        # split ids and title abs, retain order
-        doc_ids, title_abs = zip(*title_abs.items())
+    def _vectorize(self, title_abs: Tuple[str]) -> List[List[float]]:
+        """
+        get title and abstract per pdf in parquet return df columns pdf_name and specter vector
+
+        get row title and abstracts, return 768 element SPECTER vector
+        https://arxiv.org/pdf/2004.07180.pdf
+        https://github.com/allenai/specter
+        :param row: pandas DataFrame row - i.e. use df.apply(get_vectors)
+        :return: list[float] a SPECTER vector
+        """
 
         # tokenize title_abs and get model output
         inputs = self.tokenizer(list(title_abs), padding=True, truncation=True, return_tensors="pt", max_length=512)
         result = self.model(**inputs)
         # take the first token in the batch as the embedding, the SPECTER vector
-        result = result.last_hidden_state[:, 0, :].tolist()
+        return result.last_hidden_state[:, 0, :].tolist()
 
-        vector_df = pd.DataFrame.from_dict(
+    @staticmethod
+    def _build_df(doc_ids: Tuple[str], specter_vectors: List[List[float]]) -> pd.DataFrame:
+        return pd.DataFrame.from_dict(
             {
                 'pdf_name': list(doc_ids),
-                'SPECTER_vectors': result
+                'SPECTER_vectors': specter_vectors
             }
         )
-        return vector_df
+
+    def create_vectors(self):
+        """load parquet create SPECTER vectors parquet and save to disk"""
+        document_sections_df = pd.read_parquet(self.filepath)
+        single_doc_dfs = self._convert_to_single_doc_dfs(document_sections_df)
+        doc_ids, title_abs = self._extract_title_and_abstract(single_doc_dfs)
+        specter_vectors = self._vectorize(title_abs)
+        vector_df = self._build_df(doc_ids, specter_vectors)
+        vector_df.to_parquet(self.filepath.parent / (self.dataset_id + '_specter_vectors.parquet'))
 
 
 def main():
-    sp = SPECTERizer(filepath='/Users/imcconnell/UWCOSMOS/table_extraction/parquets_for_testing/',
-                     dataset_id='documents')
+    # demonstrate specter vector production and output format
+    # set up input _sections.parquet
+    filepath = Path.cwd()
+    dataset_id = 'documents'
+    input_parquet_path = filepath / (dataset_id + '_sections.parquet')
+    pd.DataFrame.from_dict(
+        {
+            'pdf_name': ['123', '123', '124', '124', '125', '125'],
+            'section_header': [None, 'abstract', None, 'Abstract', None, 'ABSTRACT'],
+            'content': ['COVID-19 research report', 'Here we summarise some COVID-19 research',
+                        'Immunization response analysis', 'mRNA goes in, antibodies come out',
+                        'COVID-19 impact on public transport', 'The wheels on the bus go round and round']
+        }
+    ).to_parquet(input_parquet_path)
+
+    # run SPECTERizer
+    sp = SPECTERizer(filepath=filepath,
+                     dataset_id=dataset_id)
     sp.create_vectors()
+
+    # show result
+    output_parquet_path = filepath / (dataset_id + '_specter_vectors.parquet')
+    print(pd.read_parquet(output_parquet_path))
+
+    # tidy up - remove input and output parquets
+    input_parquet_path.unlink()
+    output_parquet_path.unlink()
 
 
 if __name__ == "__main__":
