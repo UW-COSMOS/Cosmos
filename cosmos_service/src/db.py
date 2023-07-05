9940d7ec84218045167bf2c892f83b66161e4516
Matthew Westphall <westphall@wisc.edu>
2023-07-05 11:50:54 -0500

    add untracked files

---
+++ cosmos_service/src/db.py
@@ -0,0 +1,10 @@
+"""
+Manage a simple Sqlite DB for storing cosmos session information
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from processing_session_types import Base
+
+engine = create_engine('sqlite:///sessions.db', echo=False)
+Base.metadata.create_all(engine)
+SessionLocal = sessionmaker(bind=engine)
