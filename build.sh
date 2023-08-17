277df4fe7901843c6f9201ab7724e9c030665c8f
Ian Ross <iross@cs.wisc.edu>
2023-08-17 12:58:23 -0500

    Check for success of base and ingestion build; add service build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-export DOCKER_BUILDKIT=1
+#export DOCKER_BUILDKIT=1
 if [ -z "$1" ]
 then
     export VERSION=latest
@@ -6,8 +6,22 @@ else
     export VERSION=$1
 fi
 echo "Creating images tagged with $VERSION"
+
 docker build -t uwcosmos/cosmos-base:$VERSION -f deployment/cosmos.Dockerfile .
-docker build --build-arg VERSION=latest -t uwcosmos/cosmos-ingestion:$VERSION -f deployment/ingestion.Dockerfile .
+if [ $? -eq 0 ]; then
+    echo "Base image tagged with $VERSION"
+else
+    echo "Could not create base image! Exiting."
+    exit 1
+fi
+docker build --build-arg VERSION=$VERSION -t uwcosmos/cosmos-ingestion:$VERSION -f deployment/ingestion.Dockerfile .
+if [ $? -eq 0 ]; then
+    echo "Ingestion image tagged with $VERSION"
+else
+    echo "Could not create ingestion image! Exiting."
+    exit 1
+fi
+docker build --build-arg VERSION=$VERSION -t uwcosmos/cosmos-service:$VERSION -f deployment/cosmos-service.Dockerfile .
 #docker build --build-arg VERSION=latest -t uwcosmos/cosmos-api:$VERSION -f deployment/api.Dockerfile .
 #docker build --build-arg VERSION=latest -t uwcosmos/cosmos-retrieval:$VERSION -f deployment/retrieval.Dockerfile .
 #docker build --build-arg VERSION=latest -t uwcosmos/cosmos-linking:$VERSION -f deployment/linking.Dockerfile .
