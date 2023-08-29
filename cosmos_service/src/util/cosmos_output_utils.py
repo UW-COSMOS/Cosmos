ef896b3bbdcd483534029f14b9677012f1dd1f48
Matthew Westphall <westphall@wisc.edu>
2023-08-29 15:17:58 -0500

    clean up JSON response object for non-full-entry items

--- cosmos_service/src/util/cosmos_output_utils.py
+++ cosmos_service/src/util/cosmos_output_utils.py
@@ -1,22 +1,31 @@
 from zipfile import ZipFile
-from parquet_to_json import parquet_to_json
+from .parquet_to_json import parquet_to_json
 from fastapi import Request
 import pandas as pd
-import os
+from os import path
+from typing import List
 from io import BytesIO
 
 PARQUET_COLUMN_NAMES = {
-    "equations": ("equation_bb", "equation_page"),
-    "figures": ("objs_bb", "objs_page"),
-    "tables": ("objs_bb", "objs_page"),
+    "equations": ("equation_bb", "equation_page", ["content"]),
+    "figures": ("objs_bb", "objs_page", ["content"]),
+    "tables": ("objs_bb", "objs_page", ["content"]),
 }
 
-DEFAULT_PARQUET_COLUMN_NAMES = ("bounding_box", "page_num")
+DEFAULT_PARQUET_COLUMN_NAMES = ("bounding_box", "page_num", [])
 
 def extract_file_from_job(job, file_path: str):
     with ZipFile(f'{job.output_dir}/{job.pdf_name}_cosmos_output.zip') as zipf:
         return zipf.open(file_path)
 
+
+
+def _update_json_entry(json_entry: dict, request_path:str , exclude: List[str]):
+    for e in exclude:
+        json_entry.pop(e)
+    json_entry["img_path"] = path.join(request_path, path.split(json_entry["img_path"])[1])
+    
+    return json_entry
 
 def convert_parquet_to_json(job, parquet_path: str, request: Request):
     """
@@ -25,14 +34,16 @@ def convert_parquet_to_json(job, parquet_path: str, request: Request):
     image paths to match the full request URL
     """
 
-    (bb_column, page_column) = [
+    image_base_url = f"{request.base_url}process/{job.id}/result/images"
+
+    (bb_column, page_column, exclude) = ([
         vals for key, vals in PARQUET_COLUMN_NAMES.items() if parquet_path.endswith(f"{key}.parquet")
-    ] or DEFAULT_PARQUET_COLUMN_NAMES
+    ] or [DEFAULT_PARQUET_COLUMN_NAMES])[0]
     
     with extract_file_from_job(job, parquet_path) as parquet:
         json_data = parquet_to_json(parquet, bb_column, page_column)
     print(request.base_url)
 
-    return json_data
+    return [_update_json_entry(e, image_base_url, exclude) for e in json_data]
 
 
