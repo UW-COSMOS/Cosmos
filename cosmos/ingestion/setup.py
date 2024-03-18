8fc0e5adbe96045fda6474d01bf41787859f541c
iaross <iaross@cosmos0001.chtc.wisc.edu>
2024-03-18 15:55:14 -0500

    Pin version of Pillow

--- cosmos/ingestion/setup.py
+++ cosmos/ingestion/setup.py
@@ -15,7 +15,7 @@ setup(
     python_requires='>=3.8',
     install_requires=[
         "click",
-        "Pillow",
+        "Pillow==9.5.0",
         "opencv-python",
         "matplotlib",
         "tqdm",
